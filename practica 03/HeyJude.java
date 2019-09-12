c096197ee4c34fc0842d50ae6d9d3cd117a3dca0
dani-karen28 <daniela28@ciencias.unam.mx>
2019-09-12 11:21:23 -0500

    agregando

---
+++ practica 03/HeyJude.java
@@ -0,0 +1,357 @@
+import java.util.Scanner;
+
+
+public class HeyJude{
+ public static String HEY = "Hey Jude ";
+    public static String DON = "don't ";
+    public static String MAK = "make it bad.";
+    public static String BEA = "be afraid.";
+    public static String LET = "let me down.";
+    public static String TAK = "Take a sad song and make it better.";
+    public static String YOW = "You were made to go out and get her.";
+    public static String YOH = "You have found her, now go and get her.";
+    public static String REM = "Remember to ";
+    public static String LEH = "let her into you heart.";
+    public static String LES = "let her into your skin.";
+    public static String THE = "Then you ";
+     public static String CAN = "can start ";
+    public static String BEG = "begin ";
+    public static String TOM = "to make it better ";
+    public static String BET = "better ";
+    public static String NA  = "na ";
+
+    public static String AND = "And anytime you feel the pain, hey Jude, refrain,\n"
+	                            + "Don’t carry the world upon your shoulders.\n"
+                                    + "For well you know that it’s a fool who plays it cool\n"
+	                            + "By making his world a little colder.";
+
+    public static String SOL = "So let it out and let it in, hey Jude, begin,\n"
+	                            + "You’re waiting for someone to perform with.\n"
+	                            + "And don’t you know that it’s just you, hey jude, you’ll do,\n"
+	                            + "The movement you need is on your shoulder.";
+
+    
+
+
+   public static void main(String[] args){
+
+
+
+Scanner s = new Scanner(System.in);
+
+
+int i;
+String HEY, THE,TOM,dato,CAN,LEF, d, LEN,CAA, BEG, B,z,Z,U,R,Y;
+
+i=0;
+
+
+
+
+do
+ {
+System.out.println  (  "Hey Jude " );
+HEY = s.nextLine();
+System.out.println  (  " don't " );
+DON = s.nextLine();
+System.out.println ("INGRESE SOLO UNA MAK, BEA Ò LET");
+dato = s.nextLine();
+switch(dato){
+
+case "MAK": 
+
+System.out.println("make it bad.");
+MAK = s.nextLine();
+System.out.println("Take a sad song and make it better.");
+TAK= s.nextLine();
+System.out.println("Remember to ");
+REM= s.nextLine();
+System.out.println(" INGRESE SOLO UNA OPCION TAL COMO ESTA ESCRITO AQUI LEH O LES ");
+B = s.nextLine();
+switch(B){
+
+case "LEH": 
+System.out.println("let her into you heart.");
+LEF = s.nextLine();
+System.out.println("Then you");
+THE= s.nextLine();
+
+System.out.println("ingresar CAN O BEG");
+R = s.nextLine();
+
+if(R.equals("CAN")){
+System.out.println("can start");
+U= s.nextLine();
+
+}else if (R.equals("BEG"))
+{
+System.out.println("begin");
+BEG= s.nextLine();
+}
+
+else{
+System.out.println("ERROR");
+Y= s.nextLine();
+}
+System.out.println("to make it better ");
+TOM= s.nextLine();
+
+System.out.println   ("better better better better better waaaaa");
+LEF= s.nextLine();
+for(int a=1;a<6;a++){
+
+System.out.println  ( ("NA"));}
+
+
+break;
+
+case "LES":
+System.out.println("let her into your skin.");
+LEN = s.nextLine();
+System.out.println("Then you");
+THE= s.nextLine();
+System.out.println("ingresar CAN O BEG");
+R = s.nextLine();
+
+if(R.equals("CAN")){
+System.out.println("can start");
+U= s.nextLine();
+
+}else if (R.equals("BEG"))
+{
+System.out.println("begin");
+BEG= s.nextLine();}
+
+else{
+System.out.println("ERROR");
+Y= s.nextLine();}
+System.out.println("to make it better ");
+TOM= s.nextLine();
+
+System.out.println   ("better better better better better waaaaa");
+LEF= s.nextLine();
+for(int a=1;a<6;a++){
+
+System.out.println  ( ("NA"));}
+
+
+break;
+default: System.out.println ("acciòn invalida.");}
+break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+case "BEA": 
+
+System.out.println("be afraid.");
+BEA = s.nextLine();
+System.out.println("You were made to go out and get her.");
+YOW= s.nextLine();
+System.out.println("Remember to ");
+REM= s.nextLine();
+
+
+System.out.println(" INGRESE SOLO UNA OPCION TAL COMO ESTA ESCRITO AQUI LEH O LES ");
+B = s.nextLine();
+switch(B){
+
+case "LEH": 
+System.out.println("let her into you heart.");
+LEF = s.nextLine();
+System.out.println("Then you");
+THE= s.nextLine();
+
+System.out.println("ingresar CAN O BEG");
+R = s.nextLine();
+
+if(R.equals("CAN")){
+System.out.println("can start");
+U= s.nextLine();
+
+}else if (R.equals("BEG"))
+{
+System.out.println("begin");
+BEG= s.nextLine();
+}
+
+else{
+System.out.println("ERROR");
+Y= s.nextLine();
+}
+System.out.println("to make it better ");
+TOM= s.nextLine();
+
+System.out.println   ("better better better better better waaaaa");
+LEF= s.nextLine();
+for(int a=1;a<6;a++){
+
+System.out.println  ( ("NA"));}
+
+
+break;
+
+case "LES":
+System.out.println("let her into your skin.");
+LEN = s.nextLine();
+System.out.println("Then you");
+THE= s.nextLine();
+System.out.println("ingresar CAN O BEG");
+R = s.nextLine();
+
+if(R.equals("CAN")){
+System.out.println("can start");
+U= s.nextLine();
+
+}else if (R.equals("BEG"))
+{
+System.out.println("begin");
+BEG= s.nextLine();}
+
+else{
+System.out.println("ERROR");
+Y= s.nextLine();}
+System.out.println("to make it better ");
+TOM= s.nextLine();
+
+System.out.println   ("better better better better better waaaaa");
+LEF= s.nextLine();
+for(int a=1;a<6;a++){
+
+System.out.println  ( ("NA"));}
+
+
+break;
+default: System.out.println ("acciòn invalida.");}
+
+
+break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+case "LET":
+
+ System.out.println("let me down.");
+LET = s.nextLine();
+System.out.println("You have found her, now go and get.");
+YOH= s.nextLine();
+System.out.println("Remerber to ");
+REM= s.nextLine();
+
+
+System.out.println(" INGRESE SOLO UNA OPCION TAL COMO ESTA ESCRITO AQUI LEH O LES ");
+B = s.nextLine();
+switch(B){
+
+case "LEH": 
+System.out.println("let her into you heart.");
+LEF = s.nextLine();
+System.out.println("Then you");
+THE= s.nextLine();
+
+System.out.println("ingresar CAN O BEG");
+R = s.nextLine();
+
+if(R.equals("CAN")){
+System.out.println("can start");
+U= s.nextLine();
+
+}else if (R.equals("BEG"))
+{
+System.out.println("begin");
+BEG= s.nextLine();
+}
+
+else{
+System.out.println("ERROR");
+Y= s.nextLine();
+}
+System.out.println("to make it better ");
+TOM= s.nextLine();
+
+System.out.println   ("better better better better better waaaaa");
+LEF= s.nextLine();
+for(int a=1;a<6;a++){
+
+System.out.println  ( ("NA"));}
+
+
+break;
+
+case "LES":
+System.out.println("let her into your skin.");
+LEN = s.nextLine();
+System.out.println("Then you");
+THE= s.nextLine();
+System.out.println("ingresar CAN O BEG");
+R = s.nextLine();
+
+if(R.equals("CAN")){
+System.out.println("can start");
+U= s.nextLine();
+
+}else if (R.equals("BEG"))
+{
+System.out.println("begin");
+BEG= s.nextLine();}
+
+else{
+System.out.println("ERROR");
+Y= s.nextLine();}
+System.out.println("to make it better ");
+TOM= s.nextLine();
+
+System.out.println   ("better better better better better waaaaa");
+LEF= s.nextLine();
+for(int a=1;a<6;a++){
+
+System.out.println  ( ("NA"));}
+
+
+break;
+default: System.out.println ("acciòn invalida.");}
+
+
+break;
+
+default: System.out.println ("acciòn invalida.");
+
+}
+    
+System.out.println("SELECIOE 0 PARA REGRESAR A HEY Y CUALQUUIER OTRA TECLA PARA SALIR");
+
+i= s.nextInt();
+}while(i==0);
+
+        
+    }  
+ 
+}
+
+
